6d2d302867283a8798eea03f205926296e36ea07
Joshuan <joshuan.chel@gmail.com>
2023-02-17 13:24:45 +0100

    Add dryRun mode to saveOriginalNameToComment command

--- src/commands/saveOriginalNameToComment/index.ts
+++ src/commands/saveOriginalNameToComment/index.ts
@@ -1,14 +1,25 @@
+import { Argv } from 'yargs';
 import { exec } from '../../exiftool/index.js';
 import { seriesPromise } from '../../utils/promise.js';
 
 export const command = 'saveOriginalNameToComment <path>';
 export const description = 'Save original file name to comment in all files from path';
 
-export function builder(yargs: any) {
-    return yargs
+interface ISaveOriginNameToCommentArguments {
+    path: string;
+    dryRun: boolean;
+};
+
+export function builder(argv: Argv): Argv<ISaveOriginNameToCommentArguments> {
+    return argv
         .positional('path', {
             desc: 'Path to folder with photos',
             type: 'string',
+            demandOption: 'Folder is required parameter',
+        })
+        .option('dryRun', {
+            type: 'boolean',
+            default: false,
         });
 }
 
@@ -45,18 +56,22 @@ function buildComment(item: INameAndComment): INameAndComment {
     };
 }
 
-function updateComment(item: INameAndComment): Promise<any> {
+function updateComment(item: INameAndComment): Promise<string> {
     return exec<INameAndComment[]>(item.SourceFile, [`-Comment=${item.Comment}`])
         .then(() => `${item.FileName} comment update to: "${item.Comment}"`)
 }
 
-export function handler(argv: any) {
+function dryRunUpdateComment(item: INameAndComment): Promise<string> {
+    return Promise.resolve(`[DRY-RUN] ${item.FileName} comment will be update to: "${item.Comment}"`);
+}
+
+export function handler(argv: ISaveOriginNameToCommentArguments) {
     const names = new Set();
 
     getFileNamesAndComments(argv.path)
-        // .then(list => list.filter(filterAlreadyUpdated))
+        .then(list => list.filter(filterAlreadyUpdated))
         .then(list => list.map(buildComment))
-        .then((list) => seriesPromise(list, updateComment))
+        .then((list) => seriesPromise(list, argv.dryRun ? dryRunUpdateComment : updateComment))
         .then((data) => console.log('✅', data))
         .catch(err => console.error(err));
 };
