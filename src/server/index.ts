5b75a6a19a69f1c2877a84e984ea05ad122db232
Joshuan <joshuan.chel@gmail.com>
2023-03-04 21:38:37 +0100

    Add server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,18 @@
-console.log('server');
+import express from 'express';
+import { pinoHttp } from 'pino-http';
+
+const app = express();
+const port = 3000;
+
+const logger = pinoHttp({
+    transport: { target: 'pino-pretty' },
+});
+
+app.get('/', (req, res) => {
+    logger(req, res);
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
